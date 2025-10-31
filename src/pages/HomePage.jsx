2b77743112e5bf9b2e8fa1fc0b232c76e6c5df3b
Casper S Jensen <caspersjensen@outlook.dk>
2025-10-31 11:07:15 +0100

    Some quick filler

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import GridContainer from "../components/GridContainer/GridContainer";
 import Products from "../components/Products/Products";
+import salesstrat from "../assets/salesstrat.png"
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
@@ -10,7 +11,6 @@ const HomePage = () => {
   useEffect(() => {
     async function fetchSelected() {
       try {
-
         const ids = [1, 2, 3];
         const promises = ids.map((id) =>
           fetch(`http://casp142b.web.techcollege.dk/api/Products/${id}`).then(
@@ -27,7 +27,7 @@ const HomePage = () => {
       }
     }
 
-    fetchSelected(); 
+    fetchSelected();
   }, []);
 
   if (isLoading) return <p>Indlæser produkter...</p>;
@@ -36,18 +36,31 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
-      <GridContainer>
-        {products.map((item) => (
-          <Products
-            key={item.productId}
-            id={item.productId}
-            title={item.productName}
-            price={item.productPrice}
-            description={item.productDescription}
-            image={item.image}
-          />
-        ))}
-      </GridContainer>
+      <div>
+        <p>
+          sales strat image
+        </p>
+      </div>
+
+      <div style={{ background: "#585858", margintop: "20px" }}>
+        <p style={{ fontSize: "30px" }}>Featured</p>
+        <GridContainer>
+          {products.map((item) => (
+            <Products
+              key={item.productId}
+              id={item.productId}
+              title={item.productName}
+              price={item.productPrice}
+              description={item.productDescription}
+              image={item.image}
+            />
+          ))}
+        </GridContainer>
+      </div>
+      
+      <div>
+        <p>Some quick slogan with image</p>
+      </div>
     </div>
   );
 };
